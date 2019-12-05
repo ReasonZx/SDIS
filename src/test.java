6e42edb7ed8d37faed144c8c57372c24ce7d55d0
Diogo Martins <diogo.lm111@gmail.com>
2019-12-05 10:57:21 +0000

    Coordinates

--- src/test.java
+++ src/test.java
@@ -1,27 +1,43 @@
-import org.graphstream.algorithm.generator.Generator;
-import org.graphstream.algorithm.generator.RandomEuclideanGenerator;
+import org.graphstream.algorithm.generator.BarabasiAlbertGenerator;
 import org.graphstream.graph.*;
 import org.graphstream.graph.implementations.*;
-import static org.graphstream.algorithm.Toolkit.*;
+import org.graphstream.ui.layout.springbox.implementations.SpringBox;
+import org.graphstream.ui.view.Viewer;
+import org.graphstream.ui.view.ViewerPipe;
 
-import java.util.ArrayList;
+
+
+
+
 
 public class test {
-	public static void main(String args[]) {
-		ArrayList<Double> pos = new ArrayList<Double>();
-		Graph graph = new SingleGraph("test");
-	    Generator gen = new RandomEuclideanGenerator();
-	    gen.addSink(graph);
-	    gen.begin();
-	    for(int i=0; i<50; i++) {
-	            gen.nextEvents();
-	    }
-	    for(double j=0; j<50; j++) {
-	    	pos.add(nodePosition(j));
-	    }
-	    gen.end();
-	    graph.display(false);
-	    double i=diameter(graph);
-	    System.out.println(i);
-	}
+	 
+    public static void main(String[] args) throws Exception {
+            BarabasiAlbertGenerator gen = new BarabasiAlbertGenerator();
+            DefaultGraph g = new DefaultGraph("g");
+
+            gen.addSink(g);
+
+            gen.begin();
+            for (int i = 0; i < 1000; i++)
+            gen.nextEvents();
+            gen.end();
+
+            SpringBox box = new SpringBox();
+            Viewer v = g.display(false);
+            ViewerPipe pipe = v.newViewerPipe();
+            pipe.addAttributeSink(g);
+            v.enableAutoLayout(box);
+           
+            Thread.sleep(5000);
+            pipe.pump();
+           
+            for (Node n : g) {
+                    Object[] xy = n.getArray("xyz");
+                    double x = (Double) xy[0];
+                    double y = (Double) xy[1];
+                    org.graphstream.ui.geom.Point3 pixels = v.getDefaultView().getCamera().transformGuToPx(x, y, 0);
+                    System.out.printf("'%s': (%.3f;%.3f)\t--> (%.0f;%.0f)\n", n.getId(), x, y, pixels.x, pixels.y);
+            }
+    }
 }
