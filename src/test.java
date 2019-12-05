ba56cf16aab2d5c042859fbed1530be6acc49c73
Diogo Martins <diogo.lm111@gmail.com>
2019-12-05 09:40:51 +0000

    OLÁ

---
+++ src/test.java
@@ -0,0 +1,21 @@
+import org.graphstream.algorithm.generator.Generator;
+import org.graphstream.algorithm.generator.RandomEuclideanGenerator;
+import org.graphstream.graph.*;
+import org.graphstream.graph.implementations.*;
+import static org.graphstream.algorithm.Toolkit.*;
+
+public class test {
+	public static void main(String args[]) {
+		Graph graph = new SingleGraph("test");
+	    Generator gen = new RandomEuclideanGenerator();
+	    gen.addSink(graph);
+	    gen.begin();
+	    for(int i=0; i<50; i++) {
+	            gen.nextEvents();
+	    }
+	    gen.end();
+	    graph.display(false);
+	    double i=diameter(graph);
+	    System.out.println(i);
+	}
+}
