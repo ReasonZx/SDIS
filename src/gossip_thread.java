2ed2bdf51af1ca6d2e64817ec3149910d0836da3
Xico <xico.reasonzx@gmail.com>
2019-12-20 10:20:50 +0000

    Gossip for all neighbors

--- src/gossip_thread.java
+++ src/gossip_thread.java
@@ -1,8 +1,24 @@
+import org.graphstream.algorithm.generator.BarabasiAlbertGenerator;
+import org.graphstream.algorithm.generator.ChainGenerator;
+import org.graphstream.algorithm.generator.DorogovtsevMendesGenerator;
+import org.graphstream.algorithm.generator.Generator;
+import org.graphstream.algorithm.generator.LobsterGenerator;
+import org.graphstream.algorithm.generator.RandomEuclideanGenerator;
+import org.graphstream.algorithm.generator.RandomGenerator;
+import org.graphstream.algorithm.generator.BarabasiAlbertGenerator;
+import org.graphstream.graph.*;
+import org.graphstream.graph.implementations.*;
+import org.graphstream.ui.layout.springbox.implementations.SpringBox;
+import org.graphstream.ui.view.Viewer;
+import org.graphstream.ui.view.ViewerPipe;
+
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
-
-import org.graphstream.graph.Node;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class gossip_thread extends Thread{
 	
@@ -10,47 +26,50 @@ public class gossip_thread extends Thread{
 	private ArrayList<String> str_array;
 	private Node test_node;
 	private Iterator<Node> node_it;
+	private ReentrantLock lock;
+	private int done = 0;
+	private String tmp;
+	private Graph graph;
+	private int index;
+
 	
-	public gossip_thread(Node x,ArrayList<String> Q){
-		gossip_node=x;
+	public gossip_thread(Graph x,int i,ArrayList<String> Q, ReentrantLock locker){
+		graph=x;
 		str_array = Q;
+		lock = locker;
+		index = i;
 	}
 	
 	public void run(){
-		
-		if(gossip_node.getIndex() == 0) {
-		    gossip_node.addAttribute("ui.style", "fill-color: rgb(0,100,255); size: 15px;");
-			node_it = gossip_node.getNeighborNodeIterator();
-			while(node_it.hasNext()==true) {						//Painting( blue node 0. red neighbors)
+		if(index == 0) {
+			str_array.add(0,"work");																	// Node disseminating information
+		    node_it = graph.getNode(0).getNeighborNodeIterator();
+		    
+			while(node_it.hasNext()==true) {															//Painting( blue node 0. red neighbors)
 				test_node = node_it.next();
 				str_array.add(test_node.getIndex(),"work");
-				//test_node.addAttribute("ui.style", "fill-color: rgb(255, 0, 0); size: 15px;");
-				//System.out.println(test_node.getIndex());
 			}
 		}
-		else {
+		
+		else {																						// All other nodes
 			while(true) {
-				if(str_array.get(gossip_node.getIndex()) == "work") {
-				    gossip_node.addAttribute("ui.style", "fill-color: rgb(255,0,0); size: 15px;");
-					node_it = gossip_node.getNeighborNodeIterator();
-					while(node_it.hasNext()==true) {						//Painting( blue node 0. red neighbors)
+				if(str_array.get(index) == "work") {
+					node_it = graph.getNode(index).getNeighborNodeIterator();
+					while(node_it.hasNext()==true) {										
 						test_node = node_it.next();
 						str_array.add(test_node.getIndex(),"work");
-						//test_node.addAttribute("ui.style", "fill-color: rgb(255, 0, 0); size: 15px;");
-						//System.out.println(test_node.getIndex());
 					}
-					break;
+				break;
 				}
 				
 				else {
 					try {
-						TimeUnit.MILLISECONDS.sleep(500);
+						TimeUnit.MILLISECONDS.sleep(100);
 					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
 						e.printStackTrace();
-					}
+					}				
 				}
 			}
-		}	
+		}
 	}	
 }
--- src/test.java
+++ src/test.java
@@ -1,8 +1,11 @@
+import org.graphstream.algorithm.generator.BananaTreeGenerator;
 import org.graphstream.algorithm.generator.BarabasiAlbertGenerator;
+import org.graphstream.algorithm.generator.ChainGenerator;
 import org.graphstream.algorithm.generator.DorogovtsevMendesGenerator;
 import org.graphstream.algorithm.generator.Generator;
 import org.graphstream.algorithm.generator.LobsterGenerator;
 import org.graphstream.algorithm.generator.RandomEuclideanGenerator;
+import org.graphstream.algorithm.generator.RandomGenerator;
 import org.graphstream.algorithm.generator.BarabasiAlbertGenerator;
 import org.graphstream.graph.*;
 import org.graphstream.graph.implementations.*;
@@ -11,37 +14,79 @@ import org.graphstream.ui.view.Viewer;
 import org.graphstream.ui.view.ViewerPipe;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
 
 public class test {
 	public static void main(String args[]) throws InterruptedException {
 		
-		ArrayList<String> str_array = new ArrayList<String>();
+		ArrayList<String> str_array = new ArrayList<String>();						//Array of buffers (1 for each node)
 		Graph graph = new SingleGraph("test");
+		ReentrantLock lock = new ReentrantLock();
 		
-	    Generator gen = new DorogovtsevMendesGenerator();
+	    Generator gen = new RandomEuclideanGenerator();
 	    
 	    gen.addSink(graph);
 	    gen.begin();
 	    for(int i=0; i<100; i++) {
-	            gen.nextEvents();
+	            gen.nextEvents();													//Creating nodes
 	    }
 	    gen.end();
 	    graph.display(true);
-	    
-	    List<Thread> list_of_nodes = new ArrayList<Thread>();
+	    	    
+	    List<Thread> list_of_nodes = new ArrayList<Thread>();						//Array of threads (1 for each node)
 	    
 	    System.out.println(graph.getNodeCount());
+	    
 	    for(int i=0 ; i < graph.getNodeCount() ; i++) {
-	    	str_array.add(i, "");
-	    	list_of_nodes.add(i,new gossip_thread(graph.getNode(i), str_array));
+	    	str_array.add(i, "");													//Initializing each node buffer as empty
+	    	list_of_nodes.add(i,new gossip_thread(graph,i, str_array, lock));	//Assigning a thread for each node and passing it it's identifier and the string of arrays
 	    }
 	    
 	    for(int i=0 ; i < graph.getNodeCount() ; i++) {
 	    	list_of_nodes.get(i).start();
 	    }
+	    
+	    for(int i=0 ; i < graph.getNodeCount() ; i++) {
+	    	list_of_nodes.get(i).join();
+	    }	    
+	    graph.getNode(0).addAttribute("ui.style", "fill-color: rgb(0,100,255); size: 15px;");
+
+	    for(int i=1 ; i < graph.getNodeCount() ; i++) {
+	    	if(str_array.get(i) == "work") {
+	    		System.out.println(i);
+			    graph.getNode(i).addAttribute("ui.style", "fill-color: rgb(255,0,0); size: 15px;");
+	    	}
+	    }
+	    
+	   /* //JUST FOR TESTING NEIGHBORS
+	    
+	    Iterator<Node> node_it;
+	    Node this_node;
+	    
+	    for (int i = 0 ; i < graph.getNodeCount() ; i++) {
+	    	System.out.print(i + " - ");
+		    graph.getNode(i).addAttribute("ui.style", "fill-color: rgb(0,100,255); size: 15px;");
+	    	node_it = graph.getNode(i).getNeighborNodeIterator();
+	    	while(node_it.hasNext()) {
+		    	this_node = node_it.next();
+		    	System.out.print(this_node.getIndex() + " ");
+				this_node.addAttribute("ui.style", "fill-color: rgb(255, 0, 0); size: 15px;");
+	    	}
+	    	System.out.println();
+	    	try {
+				TimeUnit.MILLISECONDS.sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	    }
+	    */
 	    
 	    
 	    /*
