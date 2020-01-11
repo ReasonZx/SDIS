427a0df3ee78d7bccd4019da631da1664cac8493
Xico <xico.reasonzx@gmail.com>
2020-01-11 21:16:48 +0000

    Metrics

--- src/test.java
+++ src/test.java
@@ -10,6 +10,9 @@ import org.graphstream.algorithm.generator.BarabasiAlbertGenerator;
 import org.graphstream.graph.*;
 import org.graphstream.graph.implementations.*;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -18,19 +21,21 @@ import java.util.concurrent.TimeUnit;
 
 public class test {
 	
-	public static void main(String args[]) throws InterruptedException {
+	private static long startTime;
+	
+	public static void main(String args[]) throws InterruptedException, FileNotFoundException, UnsupportedEncodingException {
 		
-		
+		PrintWriter writer = new PrintWriter("results.txt", "UTF-8");
 		int bitrate = 100;
 		int packetSize = 10000;
 		int tTrans = packetSize/bitrate;
 		int timeOut = 10000;
-		double probability = 0.001;
+		double probability = 0.5;
 		
 		ArrayList<String> str_array = new ArrayList<String>();						//Array of buffers (1 for each node)
 		Graph graph = new SingleGraph("test");
 		
-	    Generator gen = new LobsterGenerator();										//Changeable Generator
+	    Generator gen = new RandomEuclideanGenerator();										//Changeable Generator
 	    
 	    gen.addSink(graph);
 	    gen.begin();
@@ -55,24 +60,81 @@ public class test {
 	    int num_over=0;
 	    boolean over_list[];
 	    over_list = new boolean[nodeCount];
+	    boolean do_once[];
+	    do_once = new boolean[11];
 	    
-	    while(num_over != nodeCount) {
+	    
+		startTime = System.currentTimeMillis();
+	    String format = "%-40s%d%s%n";
+		
+	    while(num_over != nodeCount && (System.currentTimeMillis() - startTime < timeOut-500)) {
 		    for(int i=0 ; i < nodeCount ; i++) {
-			    if(!list_of_nodes.get(i).isAlive() && over_list[i]!=true) {
+			    if(!list_of_nodes.get(i).isAlive() && !over_list[i]) {
 			    	num_over++;
 			    	over_list[i] = true;
-			    	System.out.println(num_over);
-			    };
+			    	if((double) num_over / nodeCount > 0.95 && !do_once[10]) {
+			    		System.out.printf(format,"0.95 converged in", System.currentTimeMillis() - startTime , " ms");
+			    		writer.printf(format,"0.95 converged in", System.currentTimeMillis() - startTime , " ms");
+			    		do_once[10] = true;
+			    	}
+			    	else if((double) num_over / nodeCount > 0.92 && !do_once[9]) {
+			    		System.out.printf(format,"0.92 converged in", System.currentTimeMillis() - startTime , " ms");
+			    		writer.printf(format,"0.92 converged in", System.currentTimeMillis() - startTime , " ms");
+			    		do_once[9] = true;
+			    	}
+			    	else if((double) num_over / nodeCount > 0.9 && !do_once[8]) {
+			    		System.out.printf(format,"0.9 converged in", System.currentTimeMillis() - startTime , " ms");
+			    		writer.printf(format,"0.9 converged in", System.currentTimeMillis() - startTime , " ms");
+			    		do_once[8] = true;
+			    	}
+			    	else if((double)num_over / nodeCount > 0.8 && !do_once[7]) {
+			    		System.out.printf(format,"0.8 converged in", System.currentTimeMillis() - startTime, " ms");
+			    		writer.printf(format,"0.8 converged in", System.currentTimeMillis() - startTime, " ms");
+			    		do_once[7] = true;
+			    	}
+			    	else if((double)num_over / nodeCount > 0.7 && !do_once[6]) {
+			    		System.out.printf(format,"0.7 converged in", System.currentTimeMillis() - startTime, " ms");
+			    		writer.printf(format,"0.7 converged in", System.currentTimeMillis() - startTime, " ms");
+			    		do_once[6] = true;
+			    	}
+			    	else if((double)num_over / nodeCount > 0.6 && !do_once[5]) {
+			    		System.out.printf(format,"0.6 converged in", System.currentTimeMillis() - startTime, " ms");
+			    		writer.printf(format,"0.6 converged in", System.currentTimeMillis() - startTime, " ms");
+
+			    		do_once[5] = true;
+			    	}
+			    	else if((double)num_over / nodeCount > 0.5 && !do_once[4]) {
+			    		System.out.printf(format,"0.5 converged in", System.currentTimeMillis() - startTime, " ms");
+			    		writer.printf(format,"0.5 converged in", System.currentTimeMillis() - startTime, " ms");
+
+			    		do_once[4] = true;
+			    	}
+			    	else if((double)num_over / nodeCount > 0.4 && !do_once[3]) {
+			    		System.out.printf(format,"0.4 converged in", System.currentTimeMillis() - startTime, " ms");
+			    		writer.printf(format,"0.4 converged in", System.currentTimeMillis() - startTime, " ms");
+			    		do_once[3] = true;
+			    	}
+			    	else if((double)num_over / nodeCount > 0.3 && !do_once[2]) {
+			    		System.out.printf(format,"0.3 converged in", System.currentTimeMillis() - startTime, " ms");
+			    		writer.printf(format,"0.3 converged in", System.currentTimeMillis() - startTime, " ms");
+			    		do_once[2] = true;
+			    	}
+			    	else if((double)num_over / nodeCount > 0.2 && !do_once[1]) {
+			    		System.out.printf(format,"0.2 converged in", System.currentTimeMillis() - startTime, " ms");
+			    		writer.printf(format,"0.2 converged in", System.currentTimeMillis() - startTime, " ms");
+			    		do_once[1] = true;
+			    	}
+			    	else if((double)num_over / nodeCount > 0.1 && !do_once[0]) {
+			    		System.out.printf(format,"0.1 converged in", System.currentTimeMillis() - startTime, " ms");
+			    		writer.printf(format,"0.1 converged in", System.currentTimeMillis() - startTime, " ms");
+			    		do_once[0] = true;
+			    	}
+			    }
 		    }
 	    }
-
 	    
-	    for(int i=0 ; i < nodeCount ; i++) {					
-	    	list_of_nodes.get(i).join();											//Wait for all threads to finish
-	    }	    
 	    
 	    int count = 0;
-	    System.out.println("DONE!\n");
 	    for(int i=0 ; i < nodeCount ; i++) {								// Test if every node has the message
 	    	if(str_array.get(i).contains("work")) {
 			    count++;
@@ -81,45 +143,31 @@ public class test {
 				graph.getNode(i).addAttribute("ui.style", "fill-color: rgb(255,0,0); size: 10px;");				//Painting red node that has been disseminated with information
 	    	}
 	    }
+	    if(count == nodeCount) {
+		    System.out.printf(format,"1 converged in", System.currentTimeMillis() - startTime, " ms");
+    		writer.printf(format,"1 converged in", System.currentTimeMillis() - startTime, " ms");
+	    }
+	    else {
+	    	System.out.println("100% convergence not reached");
+	    }
+
+	    System.out.println();
 	    
-	    String format = "%-40s%d%n";
+	    format = "%-40s%d%n";
 	    System.out.printf(format,"Total nodes",nodeCount);
 	    System.out.printf(format,"Disseminated nodes",count);
 	    System.out.printf(format,"Transmission time",tTrans);
 	    System.out.printf(format,"Bitrate",bitrate);
-	    System.out.printf(format,"Pakcet Size",packetSize);	    
+	    System.out.printf(format,"Pakcet Size",packetSize);	
 	    
+	    writer.println();
+	    writer.printf(format,"Total nodes",nodeCount);
+	    writer.printf(format,"Disseminated nodes",count);
+	    writer.printf(format,"Transmission time",tTrans);
+	    writer.printf(format,"Bitrate",bitrate);
+	    writer.printf(format,"Pakcet Size",packetSize);
 	    
+	    writer.close();
 	    
-	    																			//JUST FOR TESTING NEIGHBORS// CODE NOT IMPORTANT //
-	    /*
-	    Iterator<Node> node_it;
-	    Node this_node;
-	    
-	    for (int i = 0 ; i < graph.getNodeCount() ; i++) {
-	    	System.out.print(i + " - ");
-		    graph.getNode(i).addAttribute("ui.style", "fill-color: rgb(0,100,255); size: 15px;");
-	    	node_it = graph.getNode(i).getNeighborNodeIterator();
-	    	while(node_it.hasNext()) {
-		    	this_node = node_it.next();
-		    	System.out.print(this_node.getIndex() + " ");
-				this_node.addAttribute("ui.style", "fill-color: rgb(255, 0, 0); size: 15px;");
-	    	}
-	    	try {
-				TimeUnit.MILLISECONDS.sleep(1000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		    graph.getNode(i).addAttribute("ui.style", "fill-color: rgb(0,0,0); size: 15px;");
-	    	node_it = graph.getNode(i).getNeighborNodeIterator();
-		    while(node_it.hasNext()) {
-		    	this_node = node_it.next();
-		    	System.out.print(this_node.getIndex() + " ");
-				this_node.addAttribute("ui.style", "fill-color: rgb(0, 0, 0); size: 15px;");
-	    	}
-	    	System.out.println();
-	    }
-	    */
-
 	}
 }
